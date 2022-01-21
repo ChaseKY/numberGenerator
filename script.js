3defdbf5db42a1087ed51085415372c12d4d387e
Chase Young <chasekyoung@gmail.com>
2022-01-20 20:26:13 -0500

    added comments in script file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "-", "_", "+", "="];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "-", "_", "+", "="];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,3 +16,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// How long do you want the password? 8 - 100 characters
+// Do you want upper case letters?
+// Do you want lower case letters?
+// Do you want symbols?
