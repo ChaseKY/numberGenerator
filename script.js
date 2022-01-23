4441a6347683d8d91f0eee83c45a754d195d989e
Chase Young <chasekyoung@gmail.com>
2022-01-22 22:19:16 -0500

    changed arrays into strings

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "-", "_", "+", "="];
 
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*?-_+=";
+var passwordChoices = ""
 // Write password to the #password input
 function writePassword() {
   debugger
@@ -17,22 +18,35 @@ function writePassword() {
 
 function generatePassword() {
   debugger
-  var lpass = prompt("How long do you want your password?");
-  if (lpass >= 8 && lpass <= 100) {
-    var upperc = confirm("Do you want upper case letters?");
-    var lowerc = confirm("Do you want lower case letters?");
-    var numb = confirm("Do you want numbers?");
-    var symb = confirm("Do you want symbols");
+  var passwordLength = prompt("How long do you want your password?");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    confirm("Do you want upper case letters?");
+      if (true) {
+        passwordChoices += upper
+      }
+    confirm("Do you want lower case letters?");
+      if (true) {
+        passwordChoices += lower
+      }
+    confirm("Do you want numbers?");
+      if (true) {
+        passwordChoices += numbers
+      }
+    confirm("Do you want symbols");
+      if (true) {
+        passwordChoices += symbols
+      }
     return 
   } else {
-    alert("Your password length must be between 8 and 100 characters.")
+    alert("Your password length must be between 8 and 128 characters.")
   }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// How long do you want the password? 8 - 100 characters
+// How long do you want the password? 8 - 128 characters
 // Do you want upper case letters?
 // Do you want lower case letters?
 // Do you want numbers?
